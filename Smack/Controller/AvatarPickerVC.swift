9e0c6315080cdcd79f0994dd3ddd803a339337f5
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-09-25 12:12:22 +0900

    Avatar Picker Part 2

--- Smack/Controller/AvatarPickerVC.swift
+++ Smack/Controller/AvatarPickerVC.swift
@@ -47,9 +47,37 @@ class AvatarPickerVC: UIViewController, UICollectionViewDelegate, UICollectionVi
     }
     
     @IBAction func segmentControlChanged(_ sender: Any) {
+        if segmentControl.selectedSegmentIndex == 0{
+            avatarType = .dark
+        } else {
+            avatarType = .light
+        }
         
+        collectionView.reloadData()
     }
     
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        if avatarType == .dark {
+            UserDataService.instance.setAvatarName(avatarName: "dark\(indexPath.item)")
+        } else {
+            UserDataService.instance.setAvatarName(avatarName: "light\(indexPath.item)")
+        }
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        var numOfColumns : CGFloat = 3
+        if UIScreen.main.bounds.width > 320 {
+             numOfColumns = 4
+        }
+        
+        let spaceBetweenCells : CGFloat = 10
+        let padding : CGFloat = 40
+        let cellDimension = ((collectionView.bounds.width - padding) - (numOfColumns - 1) * spaceBetweenCells) / numOfColumns
+        
+        return CGSize(width: cellDimension, height: cellDimension)
+    }
     
     @IBAction func backPressed(_ sender: Any) {
         
--- Smack/Controller/CreateAccountVC.swift
+++ Smack/Controller/CreateAccountVC.swift
@@ -28,6 +28,13 @@ class CreateAccountVC: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        if UserDataService.instance.avatarName != "" {
+            userImg.image = UIImage(named: UserDataService.instance.avatarName)
+            avatarName = UserDataService.instance.avatarName        
+        }
+    }
+    
     @IBAction func createAccountPressed(_ sender: Any) {
         
         guard let name = usernameTxt.text , usernameTxt.text != "" else { return }
