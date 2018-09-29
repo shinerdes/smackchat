a686f96d2fd22b0bfdd6abbe62d1fc0d07d98b37
김영석 <yeongseok@gim-yeongseog-ui-MacBook-Pro.local>
2018-09-30 00:13:46 +0900

    fetching message

---
+++ Smack/Services/Message.swift
@@ -0,0 +1,22 @@
+//
+//  Message.swift
+//  Smack
+//
+//  Created by 김영석 on 2018. 9. 29..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import Foundation
+
+struct Message {
+    public private(set) var message: String!
+    public private(set) var userName: String!
+    public private(set) var channelId: String!
+    public private(set) var userAvatar: String!
+    public private(set) var userAvatarColor: String!
+    public private(set) var id: String!
+    public private(set) var timeStamp: String!
+
+    
+    
+}
--- Smack/Services/MessageService.swift
+++ Smack/Services/MessageService.swift
@@ -15,6 +15,7 @@ class MessageService {
     static let instance = MessageService()
     
     var channels = [Channel]()
+    var messages = [Message]()
     var selectedChannel : Channel?
     
     func findAllChannel(completion: @escaping CompletionHandler) {
@@ -33,9 +34,7 @@ class MessageService {
                     }
                     
                     NotificationCenter.default.post(name: NOTIF_CHANNELS_LOADED, object: nil)
-                    print(self.channels[0].channelTitle!)
-                    print(self.channels[1].channelTitle!)
-                  
+                   
 
                     completion(true)
                 }
@@ -50,9 +49,56 @@ class MessageService {
         
     }
     
+    func findAllMessageForChannel(channelId: String, completion: @escaping CompletionHandler) {
+        Alamofire.request("\(URL_GET_MESSAGES)\(channelId)", method: .get, parameters: nil, encoding: JSONEncoding.default, headers: BEARER_HEADER).responseJSON { (response) in
+            
+            if response.result.error == nil {
+                self.clearMessage()
+                guard let data = response.data else { return }
+                if let json = try! JSON(data: data).array {
+                    
+                    for item in json {
+                        let messageBody = item["messageBody"].stringValue
+                        let channelId = item["channelId"].stringValue
+                        let id = item["_id"].stringValue
+                        let userName = item["userName"].stringValue
+                        let userAvatar = item["userAvatar"].stringValue
+                        let userAvatarColor = item["userAvatarColor"].stringValue
+                        let timeStamp = item["timeStamp"].stringValue
+                        
+                        let message = Message(message: messageBody, userName: userName, channelId: channelId, userAvatar: userAvatar, userAvatarColor: userAvatarColor, id: id, timeStamp: timeStamp)
+                        
+                        self.messages.append(message)
+                        
+                
+                    }
+                    
+                }
+            } else {
+                debugPrint(response.result.error as Any)
+                completion(false)
+            }
+        }
+        
+        
+    }
+    
+    func clearMessage() {
+        messages.removeAll()
+    }
+    
     func clearChannels(){
         channels.removeAll()
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 
